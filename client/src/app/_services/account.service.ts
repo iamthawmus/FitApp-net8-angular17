464f12ca5c375892de8adebf40e202e34fc4d225
iamthawmus <iamthawmus@gmail.com>
2024-07-23 10:10:18 -0700

    feature(Added image uploading functionality) - used cloudinary to host images - added functionality to deleting and uploading pictures

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -18,8 +18,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map(user => {
         if(user){
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
       })
     );
@@ -34,12 +33,17 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
       map(user => {
         if(user){
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.set(user);
+         this.setCurrentUser(user);
         }
         return user;
       })
     );
   }
 
+  setCurrentUser(user: User){
+    if(user){
+      localStorage.setItem('user', JSON.stringify(user));
+      this.currentUser.set(user);
+    }
+  }
 }
