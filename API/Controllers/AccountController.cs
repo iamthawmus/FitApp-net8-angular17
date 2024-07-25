700187dcb0d9fe4601cc6a71847050584fc8be94
iamthawmus <iamthawmus@gmail.com>
2024-07-25 10:42:05 -0700

    feature(added pagnination, sorting and basic client caching) - added action filter to update last active date - added TimeAgo pipe and Date Pipe

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -30,7 +30,8 @@ public class AccountController(DataContext context, ITokenService tokenService, 
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user),
-            KnownAs = user.KnownAs
+            KnownAs = user.KnownAs,
+            Gender = user.Gender
         };
     }
 
@@ -56,7 +57,8 @@ public class AccountController(DataContext context, ITokenService tokenService, 
             Username = user.UserName,
             Token = tokenService.CreateToken(user),
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-            KnownAs = user.KnownAs
+            KnownAs = user.KnownAs,
+            Gender = user.Gender
         };    
     }
     private async Task<bool> UserExists(string username){
