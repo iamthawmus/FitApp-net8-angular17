0c469913c77c65faddc3fb3fc83ab7828c551653
iamthawmus <iamthawmus@gmail.com>
2024-08-03 10:59:10 -0700

    feature(Added Update Password feature)
    - Added endpoint to Admin Controller
    - Created member-edit-password component and added it to the member-edit component

---
+++ API/DTOs/UpdatePasswordDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dto;
+
+public class UpdatePasswordDto
+{
+    [Required]
+    public string Username {get; set;} = string.Empty;
+
+    [Required]
+    [StringLength(50, MinimumLength = 4)]
+    public string OldPassword {get; set;} = string.Empty;
+
+    [Required]
+    [StringLength(50, MinimumLength = 4)]
+    public string NewPassword {get; set;} = string.Empty;
+}
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -5,6 +5,7 @@ import { NgIf } from '@angular/common';
 import { TextInputComponent } from "../_forms/text-input/text-input.component";
 import { DatePickerComponent } from "../_forms/date-picker/date-picker.component";
 import { Router } from '@angular/router';
+import { matchValues } from '../_helpers/custom-validators';
 
 @Component({
   selector: 'app-register',
@@ -37,17 +38,11 @@ export class RegisterComponent implements OnInit {
       city: ['', Validators.required],
       country: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
-      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+      confirmPassword: ['', [Validators.required, matchValues('password')]],
     });
     this.registerForm.controls['password'].valueChanges.subscribe({
       next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
     });
-  }
-
-  matchValues(matchTo: string): ValidatorFn{
-    return(control: AbstractControl) => {
-      return control.value === control.parent?.get(matchTo)?.value ? null : {isMatching: true};
-    }
   }
 
   register() {
