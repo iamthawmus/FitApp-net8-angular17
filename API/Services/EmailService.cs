96982d98a0d6adefbbd02d1b6bb0958d430f0f4c
iamthawmus <iamthawmus@gmail.com>
2024-08-08 10:10:27 -0700

    fix(Domain name)
    - fixed from domain name by reverting back to resend.dev
    - added more error handling in register endpoint

--- API/Services/EmailService.cs
+++ API/Services/EmailService.cs
@@ -12,9 +12,9 @@ public class EmailService(IResendClient resend) : IEmailService
     public async Task<ResendResponse<Guid>> SendEmailAsync(string email, string subject, string htmlMessage)
     {
         var message = new EmailMessage();
-        message.From = "DoNotReply@thawmus.fitapp.com";
+        message.From = "DoNotReply@resend.dev";
         message.To.Add(email);
-        message.Subject = "Hello! " + subject ;
+        message.Subject = "Hello from FitApp! " + subject ;
         message.HtmlBody = "<div> 👋🏻 " + htmlMessage +"</div>";
 
        var response = await _resend.EmailSendAsync( message );
